c1df895e2f04a92d10659f089b6fbaeacd834c18
Cédric Randrianarivélo <101066947+CedricRandrianarivelo@users.noreply.github.com>
2024-02-05 11:56:05 +0200

    Add files via upload

---
+++ run_app.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 11:15:07 2024
+
+@author: cedri
+"""
+
+from tkinter import Tk
+from API_Dep_ETL_Tkinter import App
+
+if __name__ == "__main__":
+    root = Tk()
+    app = App(root)
+    root.mainloop()
